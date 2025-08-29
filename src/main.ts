5968fdf4171f0cd01eb0480bb8570fb79fc4d152
glpat-JzYZkd3eCVtjkoSKoEGT <dawit.tamiru@blockcerts.com>
2025-08-29 09:40:43 +0300

    added rernder url for cors policy on api

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 app.enableCors({
-    origin: 'http://localhost:5173', // your Vite frontend
+    origin: [
+  "http://localhost:5173",
+  "https://yaya-frontend-zu8m.onrender.com"
+];
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
